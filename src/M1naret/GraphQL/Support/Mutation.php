4bd133a512f3f3d30a36e92342475463b8431abd
Roman Murashkin <murashkin@citrus.com.ua>
2018-04-24 09:31:43 +0300

    manipulations with args

--- src/M1naret/GraphQL/Support/Mutation.php
+++ src/M1naret/GraphQL/Support/Mutation.php
@@ -2,6 +2,35 @@
 
 namespace M1naret\GraphQL\Support;
 
-class Mutation extends Field {
+class Mutation extends Field
+{
+    /**
+     * @var array
+     */
+    private $customArgs = [];
+
+    /**
+     * @return array
+     */
+    public function args() : array
+    {
+        return $this->customArgs;
+    }
+
+    /**
+     * @param array $args
+     */
+    protected function setArgs(array $args) : void
+    {
+        $this->customArgs = $args;
+    }
+
+    /**
+     * @param array $args
+     */
+    protected function pushArgs(array $args) : void
+    {
+        $this->customArgs = array_merge($this->customArgs, $args);
+    }
 
 }
