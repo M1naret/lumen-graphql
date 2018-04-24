df2575cfef2b1b2704111b05cd804ca107d1b836
Roman Murashkin <murashkin@citrus.com.ua>
2018-04-24 14:33:56 +0300

    type() to mutator

--- src/M1naret/GraphQL/Support/Mutation.php
+++ src/M1naret/GraphQL/Support/Mutation.php
@@ -2,7 +2,15 @@
 
 namespace M1naret\GraphQL\Support;
 
+use M1naret\GraphQL\Support\Facades\GraphQL;
+
 class Mutation extends Field
 {
-
+    /**
+     * @return null
+     */
+    public function type()
+    {
+        return GraphQL::type($this->get('type'));
+    }
 }
