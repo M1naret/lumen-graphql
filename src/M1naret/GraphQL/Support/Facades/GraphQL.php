506921275d816ad9ef767a590f11adf22e685bc5
Ihor Kapelyushny <kapelyushnyiy@citrus.com.ua>
2018-05-03 15:42:08 +0300

    Documentation for facade methods

--- src/M1naret/GraphQL/Support/Facades/GraphQL.php
+++ src/M1naret/GraphQL/Support/Facades/GraphQL.php
@@ -1,7 +1,16 @@
 <?php namespace M1naret\GraphQL\Support\Facades;
 
+use GraphQL\Type\Definition\ObjectType;
 use Illuminate\Support\Facades\Facade;
+use M1naret\GraphQL\Support\PaginationType;
 
+/**
+ * Class GraphQL
+ * @package M1naret\GraphQL\Support\Facades
+ *
+ * @method ObjectType type(string $typeName, bool $fresh = false)
+ * @method PaginationType paginate(string $typeName, string $customName = null)
+ */
 class GraphQL extends Facade
 {
     /**
