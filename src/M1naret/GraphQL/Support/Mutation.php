391997c4e44ce20c4dac86a4b688291fc67b5975
Ihor Kapelyushny <kapelyushnyiy@citrus.com.ua>
2018-05-04 15:23:11 +0300

    Remove default method type from base Mutation class

--- src/M1naret/GraphQL/Support/Mutation.php
+++ src/M1naret/GraphQL/Support/Mutation.php
@@ -2,15 +2,7 @@
 
 namespace M1naret\GraphQL\Support;
 
-use M1naret\GraphQL\Support\Facades\GraphQL;
-
 class Mutation extends Field
 {
-    /**
-     * @return null
-     */
-    public function type()
-    {
-        return GraphQL::type($this->get('type'));
-    }
+
 }
